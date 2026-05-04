10341cfdf4bc149e8f6f305280cdbb2656447d94
JaviGaming8 <144161549+JaviGaming8@users.noreply.github.com>
2026-05-04 14:25:07 -0600

    Cambio en la fecha

--- src/constants/eventData.js
+++ src/constants/eventData.js
@@ -2,7 +2,7 @@ const eventData = {
   name: "Javier",
   title: "Cumpleaños",
   age: 22,
-  date: "2026-05-08T20:00:00",
+  date: "2026-05-09T20:00:00",
   location: "En Mi Casa, Humildemente",
   mapsUrl: "https://www.google.com/maps/place//@19.9953305,-99.3689195,19.46z?entry=ttu&g_ep=EgoyMDI2MDQyOS4wIKXMDSoASAFQAw%3D%3D",
   whatsappNumber: "7731985638",
