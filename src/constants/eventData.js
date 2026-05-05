6f41de29192d2a5c4e20d60cd8e7b24c71025045
JaviGaming8 <144161549+JaviGaming8@users.noreply.github.com>
2026-05-04 18:25:12 -0600

    Final

--- src/constants/eventData.js
+++ src/constants/eventData.js
@@ -2,7 +2,7 @@ const eventData = {
   name: "Javier",
   title: "Cumpleaños",
   age: 22,
-  date: "2026-05-04T14:37:00",
+  date: "2026-05-09T20:00:00",
   location: "En Mi Casa, Humildemente",
   mapsUrl: "https://maps.app.goo.gl/GLBTo7myZGStgXpx5",
   whatsappNumber: "7731985638",
